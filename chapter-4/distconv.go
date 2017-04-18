480cf3f15da852884ad0ab743891250be522d55a
Jason Harrison <me@jasonharrison.us>
2017-04-18 18:32:36 -0400

    distconv.go

---
+++ chapter-4/distconv.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func ConvertToMeters(ft float64) float64 {
+	var m = ft*0.3048
+	return m
+}
+
+func main() {
+	fmt.Print("Enter a distance in feet: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	var output float64 = ConvertToMeters(input)
+	fmt.Println(input, "ft = ", output, "m")
+}

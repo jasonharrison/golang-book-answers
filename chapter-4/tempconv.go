1c238e521e4d15eb5b766b55f7967e8814a646e3
Jason Harrison <me@jasonharrison.us>
2017-04-18 18:28:04 -0400

    tempconv.go

---
+++ chapter-4/tempconv.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func ConvertToFahrenheit(f float64) float64 {
+	var c = (f - 32) * 5/9
+	return c
+}
+
+func main() {
+	fmt.Print("Enter a temperature in Fahrenheit: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	var output float64 = ConvertToFahrenheit(input)
+	fmt.Println(input, "°F = ", output, "°C")
+}
